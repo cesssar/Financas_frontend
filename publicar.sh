7140028527182bd4dcfcd9b1575e2c9935f499c9
Cesar Steinmeier <cesssar@me.com>
2023-10-01 20:25:58 -0300

    build

--- publicar.sh
+++ publicar.sh
@@ -1,5 +1,5 @@
-rm -rf ../FinancasReact_build/.
-cp -a ./app/financas/build/. ../FinancasReact_build/
+rm -rf /home/cesssar/Documents/React/FinancasReact_build/*
+cp -a /home/cesssar/Documents/React/financas_new/app/financas/build/* /home/cesssar/Documents/React/FinancasReact_build/
 git add .
 git commit -m "build"
 git push
